3006601f83d473295c16f2fa8c1cc0d6b7ef2df7
Wenchang Yang <sstxneo@WenchangdeMacBook-Pro.local>
2018-01-29 21:13:08 -0500

    Commit message

---
+++ external.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+// $('.kid').css('background','red').css('border','2px solid #333');
+$('.text-center')
+        .hide()
+        // .show()
+        // .fadeOut(2000);
+        .fadeIn(2000);
